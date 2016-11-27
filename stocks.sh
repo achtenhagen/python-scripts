88f4138d1c70ae283d6529a404f460fb496ac70f
Maurice Achtenhagen <maurice@Maurices-MacBook-Pro.local>
2016-11-27 10:19:43 -0500

    Add .gitignore file

--- stocks.sh
+++ stocks.sh
@@ -4,4 +4,4 @@ date=`date +%Y-%m-%d`
 
 wget "http://online.wsj.com/mdc/public/page/2_3021-activnyse-actives.html" -O "${date}.html"
 
-$(tidy -o "${date}.xhtml" "${date}.html")
+# $(tidy -o "${date}.xhtml" "${date}.html")
