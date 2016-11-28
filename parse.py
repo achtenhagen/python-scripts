c59ac95a5c0f52ced40bec9658264f410af90852
Maurice Achtenhagen <maurice@Maurices-MacBook-Pro.local>
2016-11-27 21:10:01 -0500

    add stock parser

--- parse.py
+++ parse.py
@@ -13,56 +13,73 @@ from cStringIO import StringIO
 from xml.parsers import expat
 from bs4 import BeautifulSoup
 
+# convert list to dictionary
+def to_dict(l):
+	keys = ['rank', 'title', 'volume', 'price', 'diff', 'perc_diff']
+	d = []
+	d.append(dict(zip(keys, l)))
+	return d
+
 def get_elms_for_atr_val(tag, atr, val):
 	lst=[]
 	elms = dom.getElementsByTagName(tag)
-
+	for node in elms:
+		for attrName, attrValue in node.attributes.items():
+			if attrName == atr and attrValue == val:
+				lst = list(filter(lambda x: x.nodeType == 1, node.childNodes))				
 	return lst
 
 # get all text recursively to the bottom
 def get_text(e):
-	lst=[]
-	# base case: node type 3 | 4
-	if e.nodeType in (3,4)
-		# get data
-		lst.append(e.data)
+	lst = []
+	if e.nodeType in (3,4):
+		if e.data != ' ':
+			return [e.data]
 	else:
-		# loop thru child nodes
-		# for each child in e.childNodes ...
+		for child in e.childNodes:
+			lst = lst + get_text(child)
 	return lst
-	
-# replace whitespace chars
-def replace_white_space(str):
-	p = re.compile(r'\s+')
-	new = p.sub(' ', str) # a lot of \n\t\t\t\t\t\t
-	return new.strip()
-
-# replace but these chars including ':'
-def replace_non_alpha_numeric(s):
-	p = re.compile(r'[^a-zA-Z0-9:-]+')
-	#   p = re.compile(r'\W+') # replace whitespace chars
-	new = p.sub(' ', s)
-	return new.strip()
 
 def extract_values(dm):
 	lst = []
-	l = get_elms_for_atr_val('table', 'class','most_actives')
-	# ............
-	#    get_text(e)
-	# ............
+	temp = []
+	l = get_elms_for_atr_val('table', 'class', 'mdcTable') # list or trs
+	l.pop(0)
+	for tr in l:
+		tds = list(filter(lambda x: x.nodeType == 1, tr.childNodes))
+		for td in tds:
+			item = get_text(td)
+			temp.append(item[0].replace('\n', ''))
+		lst.append(to_dict(temp))
+		temp = []
 	return lst
 	
 # mysql> describe most_active;
-def insert_to_db(l,tbl):
-	# ............
+def insert_to_db(l, tbl):
+	db = MySQLdb.connect(host="localhost", user="root", passwd="", db="stocks")
+	c = db.cursor()
+	c.execute("""CREATE TABLE IF NOT EXISTS `%s` (
+	`rank` VARCHAR(50),
+	`title` VARCHAR(255),
+	`volume` VARCHAR(50),
+	`price` VARCHAR(50),
+	`diff`VARCHAR(50),
+	`perc_diff` VARCHAR(50)
+	)""" % (tbl))
+	
+	for d in l:
+		query = """INSERT INTO `%s` (rank, title, volume, price, diff, perc_diff) VALUES ("%s", "%s", "%s", "%s", "%s", "%s");""" % (tbl, d['rank'], d['title'], d['volume'], d['price'], d['diff'], d['perc_diff'])
+		print query
+		c.execute(query)
+		db.commit()
+	c.close()
+	db.close()
 	return
 
 # convert to xhtml
 def html_to_xml(fn):
-	# return xhtml_file
 	xhtml_file = fn + ".xhtml"
-	cmd = "tidy -asxhtml -numeric -quiet -e -o " + xhtml_file + " " + fn + ".html"
-	os.system(cmd)
+	# os.system("tidy -asxhtml -numeric -quiet -o %s %s.html" % (xhtml_file, fn))
 	return xhtml_file
 
 def main():
@@ -72,20 +89,18 @@ def main():
 	
 	global dom
 	dom = parse(xhtml_fn)
-	
-	# print dom.getElementsByTagName('tr')
-
-	# lst = extract_values(dom)
+	lst = extract_values(dom)
 
 	# make sure your mysql server is up and running
-	# cursor = insert_to_db(lst,fn) # fn = table name for mysql
+	for stock in lst:
+		cursor = insert_to_db(stock, fn)
 
 	# l = select_from_db(cursor,fn) # display the table on the screen
 
 	# make sure the Apache web server is up and running
 	# write a PHP script to display the table(s) on your browser
 
-	# return xml
+	return True
 
 if __name__ == "__main__":
 	 main()
--- stocks.sh
+++ stocks.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 date=`date +%Y-%m-%d`
-
 wget "http://online.wsj.com/mdc/public/page/2_3021-activnyse-actives.html" -O "${date}.html"
-
-# $(tidy -o "${date}.xhtml" "${date}.html")
