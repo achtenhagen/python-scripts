fb904dfe32b59a92df699c45565ef32fccbb34ad
Maurice Achtenhagen <maurice@achtenhagen.me>
2016-11-28 09:12:12 -0500

    Finalize bash script

--- stocks.sh
+++ stocks.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-date=`date +%Y-%m-%d`
-wget "http://online.wsj.com/mdc/public/page/2_3021-activnyse-actives.html" -O "${date}.html"
+function get_stocks() {
+	date=`date +%Y-%m-%d`
+	wget "http://online.wsj.com/mdc/public/page/2_3021-activnyse-actives.html" -O "${date}.html"
+}
+
+x=0
+while [[ $x -le 60 ]]; do
+	get_stocks
+	sleep 3
+	x=`expr $x + 1`
+done
